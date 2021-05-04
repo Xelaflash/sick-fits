fa6ca13d9c8ab6d8f1f10371a16d6f5336b829c5
Alexandre Gros <xelaflash@gmail.com>
2021-05-04 11:12:29 -0400

    🚀 Add keystone user auth logic

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,5 +1,10 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 
 const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost/sick-fits';
@@ -9,26 +14,44 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add initial roles
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseUrl,
-    // TODO: add seed
-  },
-  // Keystone refers to data type as 'lists'
-  lists: createSchema({
-    // schema items go in here
-    User,
-  }),
-  ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
-  },
-  // TODO: Add value session here
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseUrl,
+      // TODO: add seed
+    },
+    // Keystone refers to data type as 'lists'
+    lists: createSchema({
+      // schema items go in here
+      User,
+    }),
+    ui: {
+      // show UI only for user who pass this test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        // the bangs permits to coerce as a bool
+        !!session?.data,
+    },
+    // TODO: Add value session here
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+  })
+);
