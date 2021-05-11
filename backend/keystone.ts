7adea2cb5b4bfee156d5101ae4f4f7a94589f461
Alexandre Gros <xelaflash@gmail.com>
2021-05-11 15:47:48 -0400

    🚀 creation of prodImg table and relationship wth products

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -7,6 +7,7 @@ import {
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
 
 const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost/sick-fits';
 
@@ -43,6 +44,7 @@ export default withAuth(
       // schema items go in here
       User,
       Product,
+      ProductImage,
     }),
     ui: {
       // show UI only for user who pass this test
