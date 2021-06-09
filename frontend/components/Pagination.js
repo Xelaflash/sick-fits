f9d2f91c91e3ab155bde76d89a7a810869d70922
Alexandre Gros <xelaflash@gmail.com>
2021-06-09 11:57:44 -0400

    start pagination feature ==> rendering links done

---
+++ frontend/components/Pagination.js
@@ -0,0 +1,49 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import Head from 'next/head';
+import Link from 'next/link';
+import PaginationStyles from './styles/PaginationStyles';
+import DisplayError from './ErrorMessage';
+import { perPage } from '../config';
+
+const PAGINATION_QUERY = gql`
+  query {
+    _allProductsMeta {
+      count
+    }
+  }
+`;
+
+export default function Pagination({ page }) {
+  const { error, loading, data } = useQuery(PAGINATION_QUERY);
+
+  if (loading) return 'Loading...';
+  if (error) return <DisplayError error={error} />;
+
+  const { count } = data._allProductsMeta;
+  const pageCount = Math.ceil(count / perPage);
+
+  return (
+    <PaginationStyles>
+      <Head>
+        <title>Sick Fits - Page {page} of ___ </title>
+      </Head>
+      {/* 1. Render the pagination links */}
+      <Link href={`/products/${page - 1}`}>
+        {/*  in next you can't put extra attributes on a Link component. You need to nest a a tag inside it */}
+        <a aria-disabled={page <= 1}>← Prev</a>
+      </Link>
+      <p>
+        Page {page} of {pageCount}{' '}
+      </p>
+      <p>{count} items in Total</p>
+      <Link href={`/products/${page + 1}`}>
+        <a aria-disabled={page >= pageCount}>Next →</a>
+      </Link>
+    </PaginationStyles>
+  );
+}
+
+// 2. Allow for dynamic routing
+// 3. Filter the products for the current page
+// 4. Deal with cache invalidation
