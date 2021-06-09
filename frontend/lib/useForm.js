d3b07132df618c2f6c47f300018783027fa80a66
Alexandre Gros <xelaflash@gmail.com>
2021-06-09 08:26:22 -0400

    fix edit bug where form has no product datas

--- frontend/lib/useForm.js
+++ frontend/lib/useForm.js
@@ -1,8 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function useForm(initial = {}) {
   // create a state object for our inputs
   const [inputs, setInputs] = useState(initial);
+
+  // in order to fix the bug on edit a Product where we do not get the values populated on the form ==> useForm initial state is an empty object. We need to watch the values with useEffect.
+
+  const initialValues = Object.values(initial).join('');
+  // join is just to have a string to check either empty string or values of the product.
+
+  useEffect(() => {
+    // this function runs when the thinks we are watching (form values) change
+    setInputs(initial);
+  }, [initialValues]);
 
   function handleChange(e) {
     let { value, name, type } = e.target;
