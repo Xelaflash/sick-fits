75b38b19839712930524dc659eeddb496b402aec
Alexandre Gros <xelaflash@gmail.com>
2021-09-14 16:52:56 -0400

    change prodendpoint (api)

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://sick-fits-backend-7vtjs.ondigitalocean.app/`;
+export const prodEndpoint = `https://api.scepsnkrs.club/`;
 export const perPage = 4;
